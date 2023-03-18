27f6c2ef4d744de9379730760cadf3785c933964
fishman123456 <den9152@yandex.ru>
2023-03-19 00:39:13 +0700

    7
    5

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
 using System.IO;
+using System.Windows.Automation;
 // дата создания 18.02.2023 время 8-23
 // https://blogadminday.ru/wpf-import-xls-xlsx-faylov/
 // пойду на лыжах кататься. нога болит пиии, грыжа опять
@@ -55,15 +56,15 @@ namespace Autocad_exel_read_and_to_create_lisp
 
             var extension = fileNames.Substring(fileNames.LastIndexOf('.'));
             // Создаем поток для чтения.
-           
+
             FileStream stream = File.Open(fileNames, FileMode.Open, FileAccess.Read);
             // В зависимости от расширения файла Excel, создаем того или иного способа прочтения.
             // Читатель для файлов с расширением *.xlsx.
             if (extension == ".xlsx")
-              edr = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                edr = ExcelReaderFactory.CreateOpenXmlReader(stream);
             //// Читатель для файлов с расширением *.xls.
             else if (extension == ".xls")
-              edr = ExcelReaderFactory.CreateBinaryReader(stream);
+                edr = ExcelReaderFactory.CreateBinaryReader(stream);
 
             var conf = new ExcelDataSetConfiguration
             {
@@ -81,52 +82,67 @@ namespace Autocad_exel_read_and_to_create_lisp
             return dtView;
         }
 
-
         // https://www.youtube.com/watch?v=JiEKqLdvnyY
         //// экспорт в excel
-        //private void btnExportExcel_Click(object sender, EventArgs e)
-        //{
-        //    try
-        //    {
-        //        Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
-        //        excel.Visible = true;
-        //        Microsoft.Office.Interop.Excel.Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
-        //        Microsoft.Office.Interop.Excel.Worksheet sheet1 = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets[1];
-        //        int StartCol = 1;
-        //        int StartRow = 1;
-        //        int j = 0, i = 0;
+        private void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            DataTable dt= new DataTable();
+           DataGrid dg = DbGrig.ItemsSource as DataGrid;
+            
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
 
-        //        //Write Headers
-        //        for (j = 0; j < dgvSource.Columns.Count; j++)
-        //        {
-        //            Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[StartRow, StartCol + j];
-        //            myRange.Value2 = dgvSource.Columns[j].HeaderText;
-        //        }
-
-        //        StartRow++;
-
-        //        //Write datagridview content
-        //        for (i = 0; i < dgvSource.Rows.Count; i++)
-        //        {
-        //            for (j = 0; j < dgvSource.Columns.Count; j++)
-        //            {
-        //                try
-        //                {
-        //                    Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[StartRow + i, StartCol + j];
-        //                    myRange.Value2 = dgvSource[j, i].Value == null ? "" : dgvSource[j, i].Value;
-        //                }
-        //                catch
-        //                {
-        //                    ;
-        //                }
-        //            }
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.ToString());
-        //    }
-        //}
+        }
     }
 
 }
+// https://www.youtube.com/watch?v=JiEKqLdvnyY
+//// экспорт в excel
+// изначальный код
+//try
+//{
+//    Microsoft.Office.Interop.excel = new Microsoft.Office.Interop.Excel.Application();
+//    excel.Visible = true;
+//    Microsoft.Office.Interop.Excel.Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+//    Microsoft.Office.Interop.Excel.Worksheet sheet1 = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets[1];
+//    int StartCol = 1;
+//    int StartRow = 1;
+//    int j = 0, i = 0;
+
+//    //Write Headers
+//    for (j = 0; j < dgvSource.Columns.Count; j++)
+//    {
+//        Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[StartRow, StartCol + j];
+//        myRange.Value2 = dgvSource.Columns[j].HeaderText;
+//    }
+
+//    StartRow++;
+
+//    //Write datagridview content
+//    for (i = 0; i < dgvSource.Rows.Count; i++)
+//    {
+//        for (j = 0; j < dgvSource.Columns.Count; j++)
+//        {
+//            try
+//            {
+//                Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[StartRow + i, StartCol + j];
+//                myRange.Value2 = dgvSource[j, i].Value == null ? "" : dgvSource[j, i].Value;
+//            }
+//            catch
+//            {
+//                ;
+//            }
+//        }
+//    }
+//}
+//catch (Exception ex)
+//{
+//    MessageBox.Show(ex.ToString());
+//}
+//        }
